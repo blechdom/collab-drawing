69dd11dddbd5cfcd77be94c1c5525981f39068ae
Kristin Galvin <blechdom@gmail.com>
2022-03-03 10:19:08 -0800

    collaborative drawing app in p5.js

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let colorPicker, sel
+let weight = 1
+
+function setup() {
+  createCanvas(800, 600)
+  background(0)
+ 
+  colorPicker = createColorPicker("white")
+
+  textAlign(CENTER);
+  sel = createSelect();
+  sel.position(55, 605);
+  sel.option('strokeWeight = 1', 1);
+  sel.option('strokeWeight = 3', 3);
+  sel.option('strokeWeight = 6', 6);
+  sel.option('strokeWeight = 9', 9);
+  sel.selected('strokeWeight = 1');
+  sel.changed(changeStrokeWeight);
+}
+
+function draw() {
+  stroke(colorPicker.color())
+  strokeWeight(weight)
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY)
+  }
+}
+
+function changeStrokeWeight() {
+  weight = sel.value()
+}
+  
